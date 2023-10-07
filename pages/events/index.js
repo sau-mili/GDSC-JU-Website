c8564c3514df380244d1314cf71fe041c2d609f8
sau-mili <146661411+sau-mili@users.noreply.github.com>
2023-10-07 23:54:16 +0530

    Revert "UpdateEvents page - beautify with proper space"
    This reverts commit 3d0ca0628627c7b2ab5e7f17b618f1de75bce07d.

--- pages/events/index.js
+++ pages/events/index.js
@@ -22,39 +22,17 @@ function EventPage() {
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "space-between",
-    gap: "20px", // Add gap between cards
+    gap: "20px",
+    marginRight: "20px", // Adjust the rightmargin 
+    marginLeft: "20px", // Adjust the bottommargin
   };
 
   const eventCardStyle = {
-    flex: "0 0 calc(33.33% - 20px)", // Equal size for all cards with padding
-    padding: "20px", // Padding inside the card
-  };
-
-  const imageContainerStyle = {
-    height: "400px",
-    overflow: "hidden",
-    position: "relative",
-  };
-
-  const overlayStyle = {
-    content: "",
-    position: "absolute",
-    top: "0",
-    left: "0",
-    right: "0",
-    bottom: "0",
-    background: "linear-gradient(transparent, rgba(0, 0, 0, 0.7))",
-    pointerEvents: "none",
-    opacity: "0",
-    transition: "opacity 0.3s ease-in-out",
-  };
-
-  const overlayEnter = (e) => {
-    e.currentTarget.querySelector(".overlay").style.opacity = "1";
-  };
-
-  const overlayLeave = (e) => {
-    e.currentTarget.querySelector(".overlay").style.opacity = "0";
+    flex: 1,
+    maxWidth: "calc(33.33% - 20px)", // Adjust the width
+    marginRight: "20px", // Adjust the rightmargin 
+    marginBottom: "20px", // Adjust the bottommargin
+    padding: "10px",
   };
 
   return (
@@ -69,11 +47,6 @@ function EventPage() {
                 description={event.description}
                 image={event.image}
                 link={event.link}
-                style={eventCardStyle}
-                imageContainerStyle={imageContainerStyle}
-                overlayStyle={overlayStyle}
-                overlayEnter={overlayEnter}
-                overlayLeave={overlayLeave}
               />
             );
           })}
@@ -84,25 +57,18 @@ function EventPage() {
 }
 
 const EventCard = (props) => (
-  <div style={props.style} className="event-card">
+  <div style={props.style}>
     <div className="bg-white rounded-lg shadow-lg">
-      <div
-        className="image-container"
-        style={props.imageContainerStyle}
-        onMouseEnter={props.overlayEnter}
-        onMouseLeave={props.overlayLeave}
-      >
-        <Image
-          src={props.image}
-          alt={props.title}
-          className="w-full h-full"
-          layout="responsive"
-          width={300}
-          height={180}
-          objectFit="cover"
-        />
-        <div className="overlay" style={props.overlayStyle}></div>
-      </div>
+   <div style={{ height: "180px", overflow: "hidden" }}>
+      <Image
+        src={props.image}
+        alt={props.title}
+        className="rounded-t-lg w-full"
+        width={300}
+        height={180}
+        objectFit="cover"
+      />
+          </div>
       <div className="p-6">
         <h3 className="text-xl font-semibold text-gray-800">{props.title}</h3>
         <p className="text-gray-600 mt-2">{props.description}</p>
